28e286852ac13ac63ffdd0bb9dd8cc9f9817e558
郭呈程 <guochengcheng@cloudwalk.cn>
2019-09-24 22:43:24 +0800

    新增ObjectUtils removeEmpty

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 常用工具类
+## API
+### ObjectUtils
+#### 操作对象工具类
+
+**1. init(target: Object) 初始化工具类**
+
+**2. val() 获取处理后的值**
+
+**3. removeEmpty() 去除对象中的非零不规范数据**
+```js
+type ObjectUtilsRemoveOptions = {
+  /**
+   * 数组key
+   */
+  arrayKeys?: string[];
+  /**
+   * 是否处理数组，默认为true
+   */
+  operateArray?: boolean;
+}
+```
+
+**4. static removeEmpty(target) 静态方法 去除对象中的非零不规范数据**
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cloneDeep from 'lodash/cloneDeep'
 
 /**
  * hello world
@@ -9,34 +10,101 @@ export function helloworld () {
   return 'hello world'
 }
 
+type ObjectTarget = {
+  [key: string]: any
+}
+
+/**
+ * 对象操作类去除对象中的非零不规范数据配置
+ */
+type ObjectUtilsRemoveOptions = {
+  /**
+   * 数组key
+   */
+  arrayKeys?: string[]
+  /**
+   * 是否处理数组，默认为true
+   */
+  operateArray?: boolean
+}
+
 /**
  * 对象操作类
  *
  * @class ObjectUtils
  */
-class ObjectUtils {
-  private target: object
+export class ObjectUtils {
+  private target: ObjectTarget
+  private static TYPE_ERROR = new Error('目标对象类型不为object')
 
   /**
    * 初始化工具类与目标对象
    *Creates an instance of ObjectUtils.
-   * @param {object} target
+   * @param {ObjectTarget} target
    * @memberof ObjectUtils
    */
-  constructor (target: object) {
-    this.target = target
+  constructor (target: ObjectTarget) {
+    if (typeof target !== 'object') throw ObjectUtils.TYPE_ERROR
+    this.target = cloneDeep(target)
   }
 
   /**
    * 初始化目标对象
    *
    * @static
-   * @param {object} target
+   * @param {ObjectTarget} target
    * @returns
    * @memberof ObjectUtils
    */
-  static init (target: object) {
+  static init (target: ObjectTarget) {
     return new this(target)
+  }
+  
+  /**
+   * 去除对象中的非零不规范数据
+   *
+   * @static
+   * @param {ObjectTarget} target
+   * @param {ObjectUtilsRemoveOptions} [options]
+   * @returns
+   * @memberof ObjectUtils
+   */
+  static removeEmpty (target: ObjectTarget, options?: ObjectUtilsRemoveOptions) {
+    return ObjectUtils.init(target).removeEmpty(options)
+  }
+
+  /**
+   * 去除对象中的非零不规范数据
+   *
+   * @param {ObjectUtilsRemoveOptions} [options={
+   *     arrayKeys: [],
+   *     operateArray: true,
+   *   }]
+   * @returns
+   * @memberof ObjectUtils
+   */
+  removeEmpty (options: ObjectUtilsRemoveOptions = {
+    arrayKeys: [],
+    operateArray: true,
+  }) {
+    const {
+      target,
+    } = this
+    const {
+      arrayKeys,
+      operateArray,
+    } = options
+    Object.keys(target).forEach((k: string) => {
+      if (typeof target[k] !== 'object' || target[k] === null) {
+        if (!target[k] && target[k] !== 0) {
+          if (arrayKeys && arrayKeys.indexOf(k) > -1) target[k] = []
+          else if (!(target instanceof Array) || operateArray) target[k] = '-'
+        }
+      } else {
+        target[k] = ObjectUtils.removeEmpty(target[k]).val()
+      }
+    })
+    return this
   }
 
   /**
